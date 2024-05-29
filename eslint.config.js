91c1bb7d07b5fc6fb747215ec4005a6e80bab37e
Denis Dezest <denis.dezest@gmail.com>
2024-05-29 11:56:16 +0200

    📦 Add eslint and custom config file

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(
+  /*eslint-disable @typescript-eslint/no-unsafe-argument */
+  /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+  // Provide base and additional configurations.
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+
+  // Define config objects (flat cascading order).
+  {
+    languageOptions: {
+      globals: { ...globals.node },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    // Customize rules.
+    rules: {
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: false,
+        },
+      ],
+      // Allow prefixing unused variables with '_'
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  // Config object to globally exclude listed files from linting.
+  {
+    ignores: ['dist', 'public', 'node_modules'],
+  },
+  eslintConfigPrettier
+);
